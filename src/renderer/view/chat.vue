91e6cd4a8e380ba6d2474fad474a7cbbf1e8e63d
Hancel Lin <imlinhanchao@gmail.com>
2021-11-18 20:47:26 +0800

    支持解析紅包消息

--- src/renderer/view/chat.vue
+++ src/renderer/view/chat.vue
@@ -217,6 +217,38 @@
 .msg-control-list {
     position: relative;
     padding-left:36px;
+}    
+.msg-current {
+    .redpacket-item {
+        flex-direction: row-reverse;
+        .arrow {
+            border-right-color: transparent;
+            border-left-color: #f90;
+        }
+    }
+}
+
+.redpacket-item {
+    display: flex;
+    flex-direction: row;
+    cursor: pointer;
+    .arrow {
+        border-right-color: #f90;
+    }
+    .redpacket-content{
+        display: inline-flex;
+        align-items: center;
+        background: #f90;
+        border-radius: 5px;
+        padding-right: 10px;
+        .redpacket-icon {
+            width: 64px;
+            height: 64px;
+        }
+        .redpacket-msg {
+            color: #FFF;
+        }
+    }
 }
 </style>
 <style lang="less">
@@ -329,6 +361,10 @@
 <template>
 <article class="layout no-drag">
     <section @click="menu = {}" class="content">
+        <symbol id="redPacketIcon" viewBox="0 0 1024 1024">
+            <path d="M705.2 445.28C689.12 536.48 608.608 606.256 512 606.256c-91.232 0-171.728-64.4-187.84-150.272l-134.16-80.496V783.36c0 59.04 48.304 101.968 101.968 101.968h440.064c59.04 0 101.968-48.288 101.968-101.968V370.128l-128.8 75.136zM512 219.856c91.232 0 166.368 64.4 187.84 150.256l134.16-85.856v-48.304c0-59.04-48.304-101.968-101.968-101.968H291.968c-53.664 0-101.968 42.928-101.968 101.968v59.04l134.16 80.48c16.112-91.216 96.608-155.616 187.84-155.616z" fill="#e6464b" p-id="4469"></path>
+            <path d="M565.664 434.528h-26.832v-21.456h26.832c16.112 0 26.832-10.736 26.832-26.832 0-16.112-10.72-26.848-26.832-26.848h-16.096l32.208-32.192c10.72-10.72 10.72-26.832 0-37.568-10.736-10.72-26.848-10.72-37.568 0L512 327.2l-32.192-37.568c-10.736-10.72-26.848-10.72-37.568 0-10.736 10.72-10.736 26.832 0 37.568l32.192 32.192h-16.096c-16.096 0-26.832 10.736-26.832 26.848 0 16.096 10.72 26.832 26.832 26.832h32.192v21.456h-32.192c-16.096 0-26.832 10.736-26.832 26.832 0 16.112 10.72 26.848 26.832 26.848h32.192v37.568c0 16.096 10.736 26.816 26.848 26.816 16.096 0 26.832-10.72 26.832-26.816v-37.568h21.456c16.112 0 26.832-10.736 26.832-26.848 0-16.096-10.72-26.832-26.832-26.832z" fill="#fecd41" opacity="1" p-id="4470"></path>
+        </symbol>
         <section class="chat-form">
             <span class="logout" @click="logout"><Avatar :src="current.userAvatarURL" title="点击注销"/></span>
             <Input ref="message"
@@ -368,7 +404,7 @@
             <section class="emoji-tab" v-if="emojiForm">
                 <div class="emoji-close" @click="emojiForm = false"><Icon custom="fa fa-times" /></div>
                 <Tabs value="emoji" type="card">
-                    <TabPane label="系统表情" name="emoji">
+                    <TabPane label="内置表情" name="emoji">
                         <article class="face-list">
                             <section class="face-item" v-for="e in emoji.list('vditor')" :title="e.name" @click="sendFace(emoji.get(e.name))">
                                 <img :src="e.url" :alt="e.name">
@@ -416,7 +452,16 @@
                             <div class="msg-menu-item" v-if="isEmoji()" title="消息中插入该表情" @click="appendMsg(null, emojiCode(item.content))">{{emojiCode(item.content)}}</div>
                             <div class="msg-menu-item" @click="quote = item">引用</div>
                         </div>
-                        <div class="msg-contain">
+                        <div class="redpacket-item" v-if="!!getRedPacket(item)">
+                            <div class="arrow" v-if="item.content.replace(/\n/g, '').match(/>[^<]+?</g)"/>
+                            <div class="redpacket-content">
+                                <svg class="redpacket-icon">
+                                    <use xlink:href="#redPacketIcon"></use>
+                                </svg>
+                                <div class="redpacket-msg">{{getRedPacket(item).msg}}</div>
+                            </div>
+                        </div>
+                        <div class="msg-contain" v-if="!getRedPacket(item)">
                             <div class="arrow" v-if="item.content.replace(/\n/g, '').match(/>[^<]+?</g)"/>
                             <div class="msg-content" v-html="formatContent(item.content)" v-if="item.content.replace(/\n/g, '').match(/>[^<]+?</g)"/>
                             <span class="msg-img" v-if="!item.content.replace(/\n/g, '').match(/>[^<]+?</g)" v-html="formatContent(item.content)"></span>
@@ -508,7 +553,7 @@
                 && (this.menuTarget || { nodeName: '' }).nodeName.toLowerCase() == 'img'
                 && this.menuTarget.className != 'emoji'
             },
-            addFace(item) {
+            addFace() {
                 if(emoji.push(null, this.menuTarget.src)) this.$Message.success('添加成功！');
                 else this.$Message.warning('表情已存在');
                 emoji.save();
@@ -559,6 +604,14 @@
                 let fileData = rsp.data.succMap;
                 let filename = Object.keys(fileData)[0]
                 this.appendMsg(null, `![${filename}](${fileData[filename]})`); 
+            },
+            getRedPacket(item) {
+                try {
+                    let data = JSON.parse(item.content.replace(/^<p>|<\/p>$/g, ''));
+                    return data;
+                } catch (e) {
+                    return false;
+                }
             },
             msgCursor() {
                 return this.$refs['message'].$el.querySelector('input').selectionStart
