eee534f7c56b261b75036eb9abbb4718c465976e
Hancel.Lin <imlinhanchao@gmail.com>
2021-12-18 21:35:58 +0800

    更新加入配置档案创建

--- src/main/block/index.js
+++ src/main/block/index.js
@@ -1,8 +1,18 @@
+import fs from 'fs'
 import windows from './windows'
 import tray from './tray'
 import events from './event'
 
+let rootPath = process.env.NODE_ENV == 'development' ? 
+    path.resolve(__dirname, '..', '..', '..') :
+    process.resourcesPath
+
 let create = (app) => {
+    if (!fs.existsSync(path.resolve(rootPath, 'config.json'))) {
+        fs.writeFileSync(path.resolve(rootPath, 'config.json'), `{
+    "domain": "pwl.icu"
+}`)
+    }
     let win = windows.create(app)
     tray.create(app, windows)
     events.create(app, windows)
